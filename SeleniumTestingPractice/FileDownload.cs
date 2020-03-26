f9b807b4ee89eb8a36c391fe5f3a2285cc430f42
Victor Queliz <Victor.Queliz@insight.com>
2020-03-26 14:53:20 -0400

    Updated File Download exercise

---
+++ SeleniumTestingPractice/FileDownload.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Xunit;
+
+namespace SeleniumSandbox.Tests.AlertsandModals
+{
+    public class FileDownloadTests : IDisposable
+    {
+        private IWebDriver _driver;
+
+        public By EnterData = By.Id("textbox");
+        public By GenerateFile = By.Id("create");
+        public By Download = By.Id("link-to-download");
+
+
+        public FileDownloadTests()
+        {
+            _driver = new ChromeDriver(".");
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "https://www.seleniumeasy.com/test/generate-file-to-download-demo.html";
+        }
+
+        public void Dispose()
+        {
+            System.Threading.Thread.Sleep(5000);
+            _driver.Quit();
+        }
+
+        [Fact]
+        public void GenerateFileTest()
+        {
+            _driver.FindElement(EnterData).SendKeys("Testing Enter Data Textarea");
+            _driver.FindElement(GenerateFile).Click();
+            _driver.FindElement(Download).Click();
+        }
+    }
+}
+
